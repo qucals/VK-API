6ac761b7935ea6a22923591f44d0dc25f5fefbe4
mauzer <totalboy@outlook.com>
2020-08-03 14:34:57 +0500

    0.0.1 version | Added examples

--- lib/cmake/vkapiConfig-release.cmake
+++ lib/cmake/vkapiConfig-release.cmake
@@ -5,15 +5,15 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "vkapi::vkapi" for configuration "Release"
-set_property(TARGET vkapi::vkapi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(vkapi::vkapi PROPERTIES
+# Import target "VKAPI::VKAPI" for configuration "Release"
+set_property(TARGET VKAPI::VKAPI APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(VKAPI::VKAPI PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/vkapi.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/VKAPI.lib"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS vkapi::vkapi )
-list(APPEND _IMPORT_CHECK_FILES_FOR_vkapi::vkapi "${_IMPORT_PREFIX}/lib/vkapi.lib" )
+list(APPEND _IMPORT_CHECK_TARGETS VKAPI::VKAPI )
+list(APPEND _IMPORT_CHECK_FILES_FOR_VKAPI::VKAPI "${_IMPORT_PREFIX}/lib/VKAPI.lib" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
